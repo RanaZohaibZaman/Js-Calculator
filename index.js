98b338814313579e20ca078090792e9eefae9fec
Rana Zohaib Zaman <zohaibzaman28@gmail.com>
2024-12-10 13:22:17 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+alert("Hi Zohaib");
+
+alert
+//Data Type
+//" " string 
+// number int 333 -333456
+
+stdobtMarks =  prompt("Obtained MaRKS")
+stdtmMarks = prompt(" Total Marks")
+res = stdobtMarks / stdtmMarks*100
+
+alert(res)
+
+document.write(res)
+
+
+
+
+
+// uint 34567897689.57689
+// bool true false 
